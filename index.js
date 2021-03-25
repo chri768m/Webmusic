bc33377a88b410c7d0ef5499213b4b3e14d41a67
MikkelYtting <Mytting1996@gmail.com>
2021-03-25 11:22:20 +0100

    dunno

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const baseUrl = ""
 Vue.createApp({
     data(){
         return {
-            hello
+            records: [],
         }
+    },
+    created(){this.helperGetAndShow(baseUrl)},
+    methods: 
+    {
+        async helperGetAndShow(url) { // helper metode: getAllCar + getByVendor are very similar
+            try {
+                const response = await axios.get(url)
+                this.records = await response.data
+            } catch (ex) {
+                alert(ex.message) // https://www.w3schools.com/js/js_popup.asp
+            }
+        },
     }
-})
+}).mount("#app")
